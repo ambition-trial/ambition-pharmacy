fa9868cf14c89b285dbe5e6dfd8b585e6d79a43b
erikvw <ew2789@gmail.com>
2018-07-05 12:30:42 +0200

    setup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/ambition-study/ambition-pharmacy',
+    url='https://github.com/ambition-trail/ambition-pharmacy',
     license='GPL license, see LICENSE',
     description='ambition protocol-specific pharmacy configurations',
     long_description=README,
